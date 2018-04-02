27ce6b5e822b10016de2dac55271f8e94cc337ef
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 07:43:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,8 +6,8 @@ function getIt(){
 }
 
 function frameIt(){
-  $('img').on('loaded', function(){
-    $(this).addClass('tasty');
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
   });
 }
 
